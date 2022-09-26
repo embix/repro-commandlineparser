4004a0db75e227f0b178e5d253f14b02251dc8c8
Michel Bretschneider <mbretsch@questnet.de>
2022-09-26 15:46:13 +0200

    initial commit
    repro for https://github.com/commandlineparser/commandline/issues/851

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using CommandLine;
+
+namespace QuickStart
+{
+    class Program
+    {
+        public class Options
+        {
+            [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+            public bool Verbose { get; set; }
+
+            [Option('p', "position", Required = false, HelpText = "Show current cursor position")]
+            public bool Position { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+                   .WithParsed<Options>(o =>
+                   {
+                       if (o.Verbose)
+                       {
+                           Console.WriteLine($"Verbose output enabled. Current Arguments: -v {o.Verbose}");
+                           Console.WriteLine("Quick Start Example! App is in Verbose mode!");
+                       }
+                       else
+                       {
+                           Console.WriteLine($"Current Arguments: -v {o.Verbose}");
+                           Console.WriteLine("Quick Start Example!");
+                       }
+
+                       if(o.Position){
+                           var pos = Console.GetCursorPosition();
+                           Console.WriteLine($"Current Cursor Position is x={pos.Left}, y={pos.Top}");
+                       }
+                   });
+        }
+    }
+}
